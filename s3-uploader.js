e1859e0b2ca788f475d089c0d96cceea74e3824a
Whelpley <whelpley@gmail.com>
2016-09-07 09:14:45 -0400

    Put access key in another file

--- s3-uploader.js
+++ s3-uploader.js
@@ -1,20 +1,15 @@
 var AWS = require('aws-sdk');
 
-// AWS.config.loadFromPath('./config.json');
+AWS.config.loadFromPath('./config.json');
 
-// need to have access to AWS in order to create an object!
-// DANGER! hard-coded credentials
+// DANGER! hard-coded credentials DELETE THIS KEY
 var s3 = new AWS.S3(
-{
-  accessKeyId: "AKIAJHTNVVNL3JJQJSDQ",
-  secretAccessKey: "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",
-  region: "us-east-1"
-}
+
 );
 
- s3.createBucket({Bucket: 'myBucketwow'}, function() {
+ s3.createBucket({Bucket: 'myBucketwowdoge'}, function() {
 
-  var params = {Bucket: 'myBucketwow', Key: 'myKeywow', Body: 'OMG!'};
+  var params = {Bucket: 'myBucketwowdoge', Key: 'suchwowdoge', Body: 'OMG! A Doge!'};
 
   s3.putObject(params, function(err, data) {
 
@@ -27,12 +22,3 @@ var s3 = new AWS.S3(
    });
 
 });
-
- // get this working
- // then try AML API -
- // building for programmers
- //objectives:
- // -should create a "training data" object - an array (probably, maybe an object) of data
- // var trainingdata = []
- //   -emulate structure from example
- //
